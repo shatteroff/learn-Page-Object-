e235cc3b88238ab6077ac7495694ef4a4ad1b321
andrei.ursaki <shatter007@mail.ru>
2019-12-09 16:55:28 +0300

    added ProductPage class and its test

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.TAG_NAME, 'h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '[class="price_color"]')
+    PRODUCT_NAME_BASKET = (By.XPATH, '//div[@class="alertinner "]/strong')
+    PRODUCT_PRICE_BASKET = (By.XPATH, '*//div[@class="alert alert-safe alert-noicon alert-info  fade in"]/*//strong')
---
+++ pages/product_page.py
@@ -0,0 +1,22 @@
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        button.click()
+
+    def should_be_same_product_in_basket(self):
+        self.should_be_name_of_product()
+        self.should_be_price_of_product()
+
+    def should_be_name_of_product(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_basket_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_BASKET).text
+        assert product_name == product_basket_name, "Product name in the basket and added are not the same"
+
+    def should_be_price_of_product(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        product_basket_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_BASKET).text
+        assert product_price == product_basket_price, "Product price in the basket and added are not the same"
